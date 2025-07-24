b643bee5282ffbb39be2982cdb713abc06a9f45d
Vladyslave07 <kolibrivlad168@gmail.com>
2025-07-24 15:07:38 +0300

    v0.0.2

---
+++ resources/js/components/PlaceholderCopart.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="text-center text-2xl font-bold">
+        Helo, this is a placeholder for Copart.
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ resources/js/pages/Copart.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import PlaceholderICopart from '@/components/PlaceholderCopart.vue';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Copart',
+        href: '/copart',
+    },
+];
+</script>
+
+<template>
+    <Head title="Copart" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
+            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+                <PlaceholderICopart />
+            </div>
+        </div>
+    </AppLayout>
+</template>
